5518c8dc8b9d00007d4a20b2b3634334b235676f
kegadev <kegadev@outlook.com>
2023-11-29 20:52:57 -0500

    Create colors.tsx

---
+++ constants/colors.tsx
@@ -0,0 +1,8 @@
+export const APP_COLORS = {
+  primary_green: "#495E57",
+  primary_yellow: "#F4CE14",
+  secondary_pink: "#EE9972",
+  secondary_soft_pink: "#FBDABB",
+  highlight_light: "#EDEFEE",
+  highlight_dark: "#333333",
+};
