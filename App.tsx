37878c9ca4c3b0ac009169d819dbe5f8a3872b2d
kegadev <kegadev@outlook.com>
2023-11-30 09:33:35 -0500

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import OnBoarding from "./screens/OnBoarding";
+import { APP_COLORS } from "./constants/colors";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <OnBoarding />
     </View>
   );
 }
@@ -13,8 +13,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: APP_COLORS.highlight_light,
   },
 });
